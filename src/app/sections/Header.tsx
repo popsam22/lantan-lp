288e3e80b7f1cd5535bdd64113a8b3bf3d156bd4
popsam22 <sobiechie7@gmail.com>
2024-10-30 18:34:05 +0100

    feat: mobile header  modal

--- src/app/sections/Header.tsx
+++ src/app/sections/Header.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import logo from "../../../public/lantanplustext@4x 1.svg";
 import hamburger from "../../../public/hamburger.svg";
+import close_button from "../../../public/close-button.svg";
+import playstore from "../../../public/btn-playstore.svg";
+import appstore from "../../../public/btn-appstore.svg";
+
+import { useState } from "react";
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleIconToggle = () => {
+    setOpen((prevstate) => !prevstate);
+  };
   return (
     <div className="flex justify-between overflow-x-hidden px-6 py-2">
       <div>
@@ -13,13 +25,16 @@ const Header = () => {
           className="object-contain"
         />
       </div>
-      <Image
-        src={hamburger}
-        alt="hamburger icon"
-        width={46}
-        height={47}
-        className="object-contain cursor-pointer"
-      />
+      <div className="md:hidden">
+        <Image
+          src={open ? close_button : hamburger}
+          alt="toggle icon"
+          width={28}
+          height={28}
+          className="object-contain cursor-pointer"
+          onClick={handleIconToggle}
+        />
+      </div>
       <div className="flex gap-16 items-center max-md:hidden">
         <div className="flex gap-10">
           <p className="text-[#3D3D3D] text-sm cursor-pointer">About</p>
@@ -33,6 +48,21 @@ const Header = () => {
           </button>
         </div>
       </div>
+      {/* Mobile view  */}
+      {open && (
+        <div className="flex flex-col h-[280px] w-full items-start p-3 absolute bg-white top-20 md:hidden transition-all duration-300">
+          <div className="flex flex-col gap-7 mb-10">
+            <p className="text-[#3D3D3D] text-sm cursor-pointer">About</p>
+            <p className="text-[#3D3D3D] text-sm cursor-pointer">Features</p>
+            <p className="text-[#3D3D3D] text-sm cursor-pointer">Team</p>
+            <p className="text-[#3D3D3D] text-sm cursor-pointer">Contact us</p>
+          </div>
+          <div className="flex gap-5">
+            <Image src={appstore} alt="appstore icon" />
+            <Image src={playstore} alt="playstore icon" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
